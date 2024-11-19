47fcad9bf7994d83535c4261ce9cb0de290d6e35
anshi06 <aradhya6768@gmail.com>
2024-11-19 17:35:03 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,22 @@
+//creating a element is core thing of react.
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "children" },
+    React.createElement("h1", {}, "I am h1 tag")
+  )
+);
+const heading = React.createElement(
+  "h1",
+  { id: "heading" }, //Responsible to give attributes to the element
+  "Hello world from React!" //Children
+);
+
+console.log(heading); //Object, In the end it is an react element.
+
+//creating the root is the job of react dom.
+const root = ReactDOM.createRoot(document.getElementById("root")); //created root for react.
+
+root.render(parent); //Append heading inside root div.
