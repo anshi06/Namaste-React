41c683187acebbd3c0a20bfdc8492c3097f5206a
anshi06 <aradhya6768@gmail.com>
2024-11-20 19:56:19 +0530

    episode-3

--- App.js
+++ App.js
@@ -11,15 +11,34 @@ const parent = React.createElement(
     React.createElement("h1", {}, "I am h1 tag")
   )
 );
+//Creating React element using core react.
 const heading = React.createElement(
   "h1",
-  { id: "heading" }, //Responsible to give attributes to the element
-  "Hello world from React!" //Children
+  { id: "heading" },
+  "Hello world from React!"
 );
 
 console.log(heading); //Object, In the end it is an react element.
 
+//JSX
+const jsxHeading = <h1 id="head">JSX Heading</h1>; //React Element using JSX
+
+console.log(jsxHeading); //Object //jsxHeading is same as heading
+
 //creating the root is the job of react dom.
 const root = ReactDOM.createRoot(document.getElementById("root")); //created root for react.
 
-root.render(parent); //Append heading inside root div.
+//Component Composition
+const Title = () => <h2>Title</h2>;
+const HeadingComponent = () => {
+  return (
+    <div>
+      <Title />
+      {Title()}
+      <Title></Title>
+      <h1>This is Heading Component.</h1>
+    </div>
+  );
+};
+
+root.render(<HeadingComponent />); //Append heading inside root div.
